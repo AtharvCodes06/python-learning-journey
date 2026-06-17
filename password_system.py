eaedd64c42df262cb6317e97468a05a76b50311a
AtharvCodes06 <khogareatharv@gmail.com>
2026-06-17 19:15:09 +0530

    Update password_system.py

--- password_system.py
+++ password_system.py
@@ -1,1 +1,15 @@
-
+password = "atharv"
+success = False
+attempt = 1
+while attempt <= 3:
+    print(f"Attempt no : {attempt}")
+    attempt += 1
+    enter_password = input("ENTER YOUR PASSWORD : ")
+    if enter_password == password:
+        print("ACCESS GRANTED")
+        success = True
+        break
+    else:
+        print("WRONG PASSWORD")
+if not success:
+ print("access denied")
